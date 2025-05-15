6299e2df5d49d481d088612321f0cbb1fa35a58c
BobFarreras <143645540+BobFarreras@users.noreply.github.com>
2025-05-15 07:29:35 +0200

    init Commit

--- app/src/main/java/com/deixebledenkaito/despertapp/MainActivity.kt
+++ app/src/main/java/com/deixebledenkaito/despertapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             DespertAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "bOBYYY",
+                        name = "bOBYYfasdfasdfaY",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
